4e6c423ec0a6e3919e092b66c80d4634a0ead3dc
beverleyngo <beverleyngo168@yahoo.co.uk>
2020-12-10 14:56:11 +0000

    Added Comments

--- day2_part1.py
+++ day2_part1.py
@@ -1,13 +1,16 @@
+# Import pandas library
 import pandas as pd
 
-#my_cols = ['int1', 'int2', 'letter', 'password']
+# Create dataframe with input file
 passwordDf = pd.read_csv('day2_1_input.csv', sep=' ', header=None, engine="python")
 
+# Format the dataframe to contain four columns
 passwordDf[['int1', 'int2']] = passwordDf[0].str.split('-', expand=True)
 passwordDf[['letter', 'colon']] = passwordDf[1].str.split(':', expand=True)
 passwordDf.rename(columns = {2: 'Password'}, inplace = True)
 passwordDf = passwordDf.drop(columns=[0,1,'colon'])
 
+# Calculate the number of valid passwords
 valid = 0
 for row in passwordDf.itertuples():
     occurences = row.Password.count(row.letter)
